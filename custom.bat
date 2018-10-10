e2584e1816fd64d026d60ee2f0542b42ef645c90
patelvijayg <patelvijayg@gmail.com>
2018-10-10 11:54:04 +0530

    change the custom file

--- custom.bat
+++ custom.bat
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 $Env:VAGRANT_VAGRANTFILE = "Vagrantfile_ArangoDB"
-=======
-$Env:VAGRANT_VAGRANTFILE = "Vagrantfile_sample"
->>>>>>> 4fa31bd00b65e89713209ac7be48f22c357611d7
 $Env:VAGRANTFILE_API_VERSION = "2"
 vagrant up
 #vagrant destroy
